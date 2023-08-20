b809deeeb3a95fcda9e841e381c55baa1df0e014
Jeremy Funk <jeremy@kombo.dev>
2023-08-20 13:46:33 +0200

    Formatting

--- src/components/toggle/index.tsx
+++ src/components/toggle/index.tsx
@@ -1,35 +1,47 @@
-import { component$, Slot, type QwikIntrinsicElements, Signal, useSignal } from "@builder.io/qwik"
+import {
+  component$,
+  Slot,
+  type QwikIntrinsicElements,
+  Signal,
+  useSignal,
+} from "@builder.io/qwik";
 
 type ToggleCustomProps = {
-	/**
-	 * The controlled state of the toggle.
-	 */
-	pressed?: Signal<boolean>
-	/**
-	 * The state of the toggle when initially rendered. Use `defaultPressed`
-	 * if you do not need to control the state of the toggle.
-	 * @defaultValue false
-	 */
-	defaultPressed?: boolean
-} 
+  /**
+   * The controlled state of the toggle.
+   */
+  pressed?: Signal<boolean>;
+  /**
+   * The state of the toggle when initially rendered. Use `defaultPressed`
+   * if you do not need to control the state of the toggle.
+   * @defaultValue false
+   */
+  defaultPressed?: boolean;
+};
 
-type ToggleProps = ToggleCustomProps & QwikIntrinsicElements["button"]
+type ToggleProps = ToggleCustomProps & QwikIntrinsicElements["button"];
 
 export const Toggle = component$((props: ToggleProps) => {
+	const {	
 	// eslint-disable-next-line qwik/use-method-usage
-	const { pressed = useSignal(props.defaultPressed), defaultPressed = false, ...buttonProps } = props
+    pressed = useSignal(props.defaultPressed),
+    defaultPressed = false,
+    ...buttonProps
+  } = props;
 
-	console.log(pressed.value)
-	return (
-		<button
-			type="button"
-			aria-pressed={pressed?.value ?? defaultPressed ?? false}
-			data-state={pressed?.value ? "on" : "off"}
-			data-disabled={props.disabled ? "" : undefined}
-			{...buttonProps as QwikIntrinsicElements["button"]}
-			onClick$={() => {pressed.value = !pressed.value}}
-		>
-			<Slot />
-		</button>
-	)
-})
+  console.log(pressed.value);
+  return (
+    <button
+      type="button"
+      aria-pressed={pressed?.value ?? defaultPressed ?? false}
+      data-state={pressed?.value ? "on" : "off"}
+      data-disabled={props.disabled ? "" : undefined}
+      {...(buttonProps as QwikIntrinsicElements["button"])}
+      onClick$={() => {
+        pressed.value = !pressed.value;
+      }}
+    >
+      <Slot />
+    </button>
+  );
+});
