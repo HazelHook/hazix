09c37b21a333c5385fa590348b0c77e72fee32ea
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-08-20 22:18:25 +0200

    feat: add avatar

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,7 @@
 import { Logo } from "./components/logo/logo"
 
 import * as Accordion from "./components/accordion"
+import * as Avatar from "./components/avatar"
 
 import "./global.css"
 import { Toggle } from "./components/toggle"
@@ -30,9 +31,35 @@ export default component$(() => {
 					</Accordion.Item>
 				</Accordion.Root>
 				<Toggle>Uncontrolled</Toggle>
-				<Toggle pressed={pressed}>
+				<Toggle
+					class="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-zinc-600 hover:text-zinc-100 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-blue-500 data-[state=on]:text-white"
+					pressed={pressed}
+				>
 					Controlled
 				</Toggle>
+				<div class="flex gap-5">
+					<Avatar.Root class="bg-blackA3 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+						<Avatar.Image
+							class="h-full w-full rounded-[inherit] object-cover"
+							src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+							alt="Colm Tuite"
+							width={64}
+							height={64}
+						/>
+						<Avatar.Fallback
+							class="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
+							delayMs={600}
+						>
+							CT
+						</Avatar.Fallback>
+					</Avatar.Root>
+
+					<Avatar.Root class="bg-blackA3 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+						<Avatar.Fallback class="text-black leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium">
+							PD
+						</Avatar.Fallback>
+					</Avatar.Root>
+				</div>
 			</body>
 		</>
 	)
