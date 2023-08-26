adc7c256c7569f5f6f9d77079b3ca6d5e1f12076
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-08-26 23:50:46 +0200

    xd

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * as Accordion from "./components/accordion"
 export * as AspectRatio from "./components/aspect-ratio"
 export * as Avatar from "./components/avatar"
 
+export * as Dialog from "./components/dialog"
+
 export * as Checkbox from "./components/checkbox"
 export * as Popover from "./components/popover"
 export * as Progress from "./components/progress"
