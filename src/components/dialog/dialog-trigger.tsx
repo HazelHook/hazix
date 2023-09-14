db16325d92d102beedc7d48356e64a246f0e07cf
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-09-14 21:01:07 +0200

    types

--- src/components/dialog/dialog-trigger.tsx
+++ src/components/dialog/dialog-trigger.tsx
@@ -1,39 +1,40 @@
-import { component$, $, Slot, type QwikIntrinsicElements } from "@builder.io/qwik"
+import { component$, $, Slot, type QwikIntrinsicElements, Component } from "@builder.io/qwik"
 import { usePortalProviderContext } from "./dialog-context"
 import { getState } from "utils/index"
 
-type AsProp<T extends keyof QwikIntrinsicElements> = {
-	as?: T
-}
+type AsProp<T extends {} | keyof QwikIntrinsicElements> = T extends keyof QwikIntrinsicElements
+	? { as?: QwikIntrinsicElements[T] }
+	: { as?: Component<T> }
 
-export type DialogTriggerProps<T extends keyof QwikIntrinsicElements> = AsProp<T> & QwikIntrinsicElements[T]
+export type DialogTriggerProps<T extends {} | keyof QwikIntrinsicElements> = AsProp<T> &
+	T extends keyof QwikIntrinsicElements
+	? QwikIntrinsicElements[T]
+	: T
 
-const DialogTrigger = component$(
-	({ as: Tag = "button" as any, ...props }: DialogTriggerProps<keyof QwikIntrinsicElements>) => {
-		const portalContext = usePortalProviderContext()
+const DialogTrigger = component$(<T extends {}>({ as: Tag = "button" as any, ...props }: DialogTriggerProps<T>) => {
+	const portalContext = usePortalProviderContext()
 
-		const onClick = $(() => {
-			if (portalContext.openSig) {
-				portalContext.openSig.value = !portalContext.openSig.value
-			}
-		})
+	const onClick = $(() => {
+		if (portalContext.openSig) {
+			portalContext.openSig.value = !portalContext.openSig.value
+		}
+	})
 
-		const Comp = Tag as any
+	const Comp = Tag as any
 
-		return (
-			<Comp
-				tabIndex={-1}
-				aria-haspopup="dialog"
-				aria-expanded={portalContext.openSig.value}
-				data-state={getState(portalContext.openSig.value)}
-				onClick$={onClick}
-				{...props}
-			>
-				<Slot />
-			</Comp>
-		)
-	},
-)
+	return (
+		<Comp
+			tabIndex={-1}
+			aria-haspopup="dialog"
+			aria-expanded={portalContext.openSig.value}
+			data-state={getState(portalContext.openSig.value)}
+			onClick$={onClick}
+			{...props}
+		>
+			<Slot />
+		</Comp>
+	)
+})
 
 const Trigger = DialogTrigger
 
